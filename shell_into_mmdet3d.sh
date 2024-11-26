f45638ccf0489574a5004b3678a43140e82e000e
Michal Sejak <hyas@seznam.cz>
2024-11-26 19:29:19 +0100

    Simplify Docker run command in shell_into_mmdet3d.sh by removing X11 access configuration

--- shell_into_mmdet3d.sh
+++ shell_into_mmdet3d.sh
@@ -9,16 +9,5 @@ if [[ "$1" == "--build" ]]; then
     fi
 fi
 
-# Allow Docker to access the host's X11 server
-xhost +local:docker
-
-# Run the container with GPU support and X11 mapping
-docker run --gpus all -it --rm \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="/dev/dri:/dev/dri" \
-    mmdet3d-container /bin/bash
-
-# Revoke X11 access after the container stops
-xhost -local:docker
+# Run the container with GPU support
+docker run --gpus all -it --rm --shm-size=8g mmdet3d-container /bin/bash
